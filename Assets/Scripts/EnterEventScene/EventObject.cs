71af681c9dfef4209be56271e08ee6e7d1e2437f
bchung2017 <chung3@cooper.edu>
2020-11-10 22:52:10 -0500

    event scene scripts 2

---
+++ Assets/Scripts/EnterEventScene/EventObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventObject
+{
+    public string eventId { get; set; }
+    public string numOfOrgs { get; set; }
+    public string uid { get; set; }
+}
